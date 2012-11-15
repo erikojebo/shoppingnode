4aef2ba24198dc4f5bb18ae48c411b4093562861
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-15 19:24:04 +0100

    .

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ var foo = "";
 
 pg.connect(process.env.DATABASE_URL, function(err, client) {
 
-    foo = "error?";
-    if (err) {
-        foo += err;
-        return;
-    }
+    // foo = "error?";
+    // if (err) {
+    //     foo += err;
+    //     return;
+    // }
 
-    foo += "executing query";
+    // foo += "executing query";
 
-    var query = client.query('SELECT * FROM List');
+    // var query = client.query('SELECT * FROM List');
 
-    query.on('row', function(row) {
-        foo += JSON.stringify(row);
-    });
+    // query.on('row', function(row) {
+    //     foo += JSON.stringify(row);
+    // });
 });
 
 var express = require('express');
