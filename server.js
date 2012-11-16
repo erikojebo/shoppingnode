8818e92012f2b5204d50e45a1224abe639a62b9f
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-16 23:17:26 +0100

    Made server serve static files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log("Starting...");
 
 var pg = require('pg');
+var fs = require('fs');
 
 if (process.env.ENVIRONMENT === "development") {
     console.log("dev environment detected");
@@ -26,11 +27,47 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-    response.send("foo: " + foo);
+app.get('/', function (request, response) {
+    fs.readFile('./list.html', function(error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
+});
+
+app.get('/list.css', function (request, response) {
+    fs.readFile('./list.css', function(error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/css' });
+            response.end(content, 'utf-8');
+        }
+    });
+});
+
+app.get('/list.js', function (request, response) {
+    fs.readFile('./list.js', function(error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/javascript' });
+            response.end(content, 'utf-8');
+        }
+    });
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
