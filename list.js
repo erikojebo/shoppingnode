ad752472393c962e5910082a64d182b4db871a1f
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-18 20:08:51 +0100

    Added constructor for item view models as well

--- list.js
+++ list.js
@@ -7,40 +7,44 @@ var CategoryViewModel = function(id, items) {
     self.name = ko.computed(function () {
 	    return "category" + self.id;
     })
-}
+};
+
+var ItemViewModel = function (item) {
+	this.id = item.id;
+    this.category = ko.observable(item.category);
+    this.isDone = ko.observable(item.done),
+    this.description = ko.observable(item.description);
+    this.toggleIsDone = function () {
+	    this.isDone(!this.isDone());
+    };
+};
 
 $(document).ready(function () {
     $.getJSON('/items', function(data) {
-
-        var itemViewModels = data.map(function (item) {
-	        return {
-                id: item.id,
-                category: ko.observable(item.category),
-                isDone: ko.observable(item.done),
-                description: ko.observable(item.description),
-                toggleIsDone: function () {
-	                this.isDone(!this.isDone());
-                }
-            }
-        });
-
-        var categoryIds = _.uniq(itemViewModels.map(function (item) {
-            return item.category();
-        }));
-
-        var categoryViewModels = categoryIds.sort().map(function (id) {
-
-            var items = itemViewModels.filter(function (item) {
-	            return item.category() == id;
-            });
-
-            return new CategoryViewModel(id, items);
-        })
-        
-        shoppingListViewModel = {
-            categories: ko.observableArray(categoryViewModels)
-        };
-
+        shoppingListViewModel = createShoppingListViewModel(data);
         ko.applyBindings(shoppingListViewModel);
     });
-})
+});
+
+function createShoppingListViewModel (data) {
+	var itemViewModels = data.map(function (item) {
+	    return new ItemViewModel(item);
+    });
+
+    var categoryIds = _.uniq(itemViewModels.map(function (item) {
+        return item.category();
+    }));
+
+    var categoryViewModels = categoryIds.sort().map(function (id) {
+
+        var items = itemViewModels.filter(function (item) {
+	        return item.category() == id;
+        });
+
+        return new CategoryViewModel(id, items);
+    })
+    
+    return {
+        categories: ko.observableArray(categoryViewModels)
+    };
+}
