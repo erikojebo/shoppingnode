3f87240ef0512ac46eaaceb13265b4b7c593d388
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-19 20:54:09 +0100

    Made categories different colors and made js use local files instead of cdn

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ var app = express.createServer(express.logger());
 addFileRoute('/', './list.html');
 addFileRoute('/list.js');
 addFileRoute('/list.css');
+addFileRoute('/lib/underscore-min.js');
+addFileRoute('/lib/jquery-1.8.3.min.js');
+addFileRoute('/lib/knockout-2.2.0.js');
 
 app.get('/items', function (request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json'});
