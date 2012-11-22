a6aa1fa5419fdf96e8afafea2cf20e7caafc8208
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-22 22:44:06 +0100

    Added sql script for generating test data

---
+++ sql/create_test_data.sql
@@ -0,0 +1,20 @@
+insert into list (description, done, category) values ('Mjölk', false, 2);
+insert into list (description, done, category) values ('Yoghurt', false, 2);
+insert into list (description, done, category) values ('Creme fraiche', false, 2);
+insert into list (description, done, category) values ('Smör', false, 2);
+
+insert into list (description, done, category) values ('Tandkräm', true, 0);
+insert into list (description, done, category) values ('Blöjor', true, 0);
+insert into list (description, done, category) values ('Våtservetter', false, 0);
+
+insert into list (description, done, category) values ('Gurka', false, 1);
+insert into list (description, done, category) values ('Bananer', false, 1);
+insert into list (description, done, category) values ('Tomater', false, 1);
+insert into list (description, done, category) values ('Paprika', false, 1);
+
+insert into list (description, done, category) values ('Ost', false, 3);
+insert into list (description, done, category) values ('Fetaost', false, 3);
+insert into list (description, done, category) values ('Mozzarella', false, 3);
+
+insert into list (description, done, category) values ('Krossade tomater', false, 4);
+insert into list (description, done, category) values ('Bröd', false, 4);
