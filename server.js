9f3584f3c25fa67b0a58186ff3eff6ef3a2fd1a3
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-24 21:03:02 +0100

    removed unused code and set title

--- server.js
+++ server.js
@@ -19,19 +19,6 @@ if (process.env.ENVIRONMENT === "development") {
 }
 
 addFileRoute('/', './client/list.html');
-// addFileRoute('/list.js');
-// addFileRoute('/list.css');
-// addFileRoute('/lib/underscore-min.js');
-// addFileRoute('/lib/jquery-1.8.3.min.js');
-// addFileRoute('/lib/knockout-2.2.0.js');
-// addFileRoute('/fonts/metrophobic/metrophobic.ttf');
-// addFileRoute('/fonts/sanchez/regular.ttf');
-// addFileRoute('/images/delete.png');
-// addFileRoute('/images/add.png');
-// addFileRoute('/images/metro_add_128.png');
-// addFileRoute('/images/metro_delete_128.png');
-// addFileRoute('/images/favs.png');
-// addFileRoute('/images/refresh.png');
 
 app.use('/images', express.static(__dirname + '/client/images'));
 app.use('/fonts', express.static(__dirname + '/client/fonts'));
